098389c2eedeba769e726225cf73c763b2999a3e
Cristian Ceballos <cristian.ceballos@masglobalconsulting.com>
2022-04-29 15:15:02 -0500

    [ADD] files

---
+++ JS/JSCODE/jsConcepts.js
@@ -0,0 +1,12 @@
+// Change variables
+var a = 1; // 1
+var a = "hello"; //hello
+var a = true; // true
+var a = false; // false
+var a = {   type: "hello",  num: 2 }; // {type: "hello", num: 2}
+
+// Operators: js operators know where create the operation:
+var a = 10+2*4; // 18
+var a = (10-2)*4; // 32
+var a = 6;
+var b = 4;
---
+++ JS/extensions.js
@@ -0,0 +1,5 @@
+//To create a console.log to a variable: ctr + alt + l:
+const result = 4;
+console.log("🚀 ~ file: extensions.js ~ line 2 ~ result", result)
+
+const val = result / 4 //?
