6cdce5b26c990650245d40cd791dda33ceec255d
cdceballor <cdceballor@eafit.edu.co>
2022-05-10 20:18:52 -0500

    [add[ info

---
+++ JS/JSCODE/loops.js
@@ -0,0 +1,11 @@
+// While loop:
+let a = 1;
+do {
+    a++;
+    console.log(a)
+} while (a < 10);
+
+// For loop:
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
---
+++ TS/loops.ts
@@ -0,0 +1,10 @@
+function loops(){
+    let a:number = 0;
+    do {
+        a++;
+    }while(a < 10);
+
+    for (let i:number = 0; i <10; i++){
+        console.log(i);
+    }
+}
